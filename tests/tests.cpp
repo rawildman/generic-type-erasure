cab59fbbc3fbefb035b9752655e343cde2d7837d
rawildman <22531519+rawildman@users.noreply.github.com>
2024-08-22 14:08:27 -0400

    Adds cmake and catch2 for testing

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,6 +1,6 @@
 #include "generic-type-erasure.hpp"
 
-#include <cassert>
+#include <catch2/catch_test_macros.hpp>
 #include <iostream>
 
 struct CopyMoveCounter
@@ -55,16 +55,15 @@ struct FortyTwo{};
 struct FortyThree{};
 struct CopyCounter{};
 
-void test_wrapper()
+TEST_CASE( "Wrapper", "[wrapper]" )
 {
   const auto t = Tester{};
   const auto wrapper = generic::TypeErased<FortyTwo, int()>{t, &Tester::forty_two};
-  assert(wrapper.call<FortyTwo>() == 42);
+  REQUIRE(wrapper.call<FortyTwo>() == 42);
 }
 
-void static_assertion_tests_signature_helper()
+TEST_CASE( "SignatureHelper static assertions", "[signaturehelper]" )
 {
-
   using TestIntVoid = generic::detail::SignatureHelper<int()>;
   static_assert(std::is_same_v<int, typename TestIntVoid::ReturnType>);
   static_assert(std::is_same_v<std::tuple<>, typename TestIntVoid::ArgTypes>);
@@ -78,7 +77,7 @@ void static_assertion_tests_signature_helper()
   static_assert(std::is_same_v<std::tuple<int, double>, typename TestVoidIntDouble::ArgTypes>);
 }
 
-void static_assertion_tests_wrapper_member_function()
+TEST_CASE( "Wrapper member function signature helper static assertions", "[signaturehelper]" )
 {
   using TestWrapperMemFun = generic::detail::WrapperConstMemberFunctionSignature<int(double, int)>;
   using ReturnFromWrapper = typename generic::detail::SignatureHelper<typename TestWrapperMemFun::Signature>::ReturnType;
@@ -87,7 +86,7 @@ void static_assertion_tests_wrapper_member_function()
   static_assert(std::is_same_v<std::tuple<const std::any&, const std::any&, std::tuple<double, int>&&>, ArgsFromWrapper>);
 }
 
-void static_assertion_tests_mem_fun_sig_helper()
+TEST_CASE( "Member function signature helper static assertions", "[signaturehelper]" )
 {
   using TestConst = generic::detail::MemberFunctionSignatureHelper<decltype(&Tester::forty_two)>;
   static_assert(std::is_same_v<typename TestConst::Name, Tester>);
@@ -102,61 +101,50 @@ void static_assertion_tests_mem_fun_sig_helper()
   static_assert(!TestNonConst::is_const);
 }
 
-void test_copy_counter()
+TEST_CASE( "Copy counts", "[wrapper]" )
 {
   auto copy_move_counter = CopyMoveCounter{};
-  assert(copy_move_counter.copies == 0);
-  assert(copy_move_counter.moves == 0);
+  REQUIRE(copy_move_counter.copies == 0);
+  REQUIRE(copy_move_counter.moves == 0);
 
   const auto& cm_counter_ref = copy_move_counter;
-  assert(cm_counter_ref.copies == 0);
-  assert(cm_counter_ref.moves == 0);
+  REQUIRE(cm_counter_ref.copies == 0);
+  REQUIRE(cm_counter_ref.moves == 0);
 
   const auto cm_counter_copy = copy_move_counter;
-  assert(cm_counter_copy.copies == 1);
-  assert(cm_counter_copy.moves == 0);
+  REQUIRE(cm_counter_copy.copies == 1);
+  REQUIRE(cm_counter_copy.moves == 0);
 
   const auto cm_counter_move = std::move(copy_move_counter);
-  assert(cm_counter_move.copies == 0);
-  assert(cm_counter_move.moves == 1);
+  REQUIRE(cm_counter_move.copies == 0);
+  REQUIRE(cm_counter_move.moves == 1);
 }
 
-void test_te_wrapper_copy_move()
+TEST_CASE( "Wrapper copy moves", "[wrapper]" )
 {
   const auto t = Tester{};
   const auto wrapper_const_ref_arg = generic::TypeErased<CopyCounter, std::pair<unsigned, unsigned>(const CopyMoveCounter&)>{t, &Tester::const_ref_arg};
   auto counter = CopyMoveCounter{};
-  assert(wrapper_const_ref_arg.call<CopyCounter>(counter).first == 0);
-  assert(wrapper_const_ref_arg.call<CopyCounter>(counter).second == 0);
+  REQUIRE(wrapper_const_ref_arg.call<CopyCounter>(counter).first == 0);
+  REQUIRE(wrapper_const_ref_arg.call<CopyCounter>(counter).second == 0);
 
   const auto wrapper_value_arg = generic::TypeErased<CopyCounter, std::pair<unsigned, unsigned>(CopyMoveCounter)>{t, &Tester::value_arg};
-  assert(wrapper_value_arg.call<CopyCounter>(counter).first == 1);
-  assert(wrapper_value_arg.call<CopyCounter>(counter).second == 2);
+  REQUIRE(wrapper_value_arg.call<CopyCounter>(counter).first == 1);
+  REQUIRE(wrapper_value_arg.call<CopyCounter>(counter).second == 2);
 
   const auto wrapper_r_value_ref_arg = generic::TypeErased<CopyCounter, std::pair<unsigned, unsigned>(CopyMoveCounter)>{t, &Tester::r_value_ref_arg};
   auto counter_for_r_values_1 = CopyMoveCounter{};
-  std::cout << "Calling for r value ref 1\n";
-  assert(wrapper_r_value_ref_arg.call<CopyCounter>(std::move(counter_for_r_values_1)).first == 0);
+  REQUIRE(wrapper_r_value_ref_arg.call<CopyCounter>(std::move(counter_for_r_values_1)).first == 0);
   auto counter_for_r_values_2 = CopyMoveCounter{};
-  std::cout << "Calling for r value ref 2\n";
-  assert(wrapper_r_value_ref_arg.call<CopyCounter>(std::move(counter_for_r_values_2)).second == 2);
+  REQUIRE(wrapper_r_value_ref_arg.call<CopyCounter>(std::move(counter_for_r_values_2)).second == 2);
 }
 
-void test_non_const_ref()
+TEST_CASE( "Non-const ref", "[wrapper]" )
 {
   auto t = Tester{};
   auto wrapper = generic::TypeErased<FortyTwo, int(int)>{t, &Tester::set_forty_two};
 
-  assert(wrapper.call<FortyTwo>(43) == 42);
-  assert(wrapper.call<FortyTwo>(44) == 43);
-}
-
-int main()
-{
-  //test_forward_as_tuple();
-  test_wrapper();
-  test_non_const_ref();
-  test_copy_counter();
-  test_te_wrapper_copy_move();
+  REQUIRE(wrapper.call<FortyTwo>(43) == 42);
+  REQUIRE(wrapper.call<FortyTwo>(44) == 43);
 }
 
